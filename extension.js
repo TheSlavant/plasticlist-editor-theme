92bddd5c8e87bcd356fe6b31ab688599362b8e7f
Yaroslav <shipilov@umich.edu>
2024-12-29 00:41:05 -0800

    Update

--- extension.js
+++ extension.js
@@ -3,10 +3,6 @@ const fs = require('fs');
 const path = require('path');
 
 function activate(context) {
-  // 3. Optionally, in desktop: if the above web approach doesn't work,
-  //    the user might still need the font installed locally. This at least
-  //    tries to load it in a background webview.
-
   // This extension doesn't need to do anything else to activate.
 }
 
